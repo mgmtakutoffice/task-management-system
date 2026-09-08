e4f434d8326a0bcc7f579460d32544a95d737cae
mgmtakutoffice <mgmt.akutoffice@gmail.com>
2026-09-08 13:52:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -206,4 +206,4 @@ class Config:
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"
     SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", "false").lower() == "true"
-    PERMANENT_SESSION_LIFETIME = timedelta(hours=12)
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=4)
